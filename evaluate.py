e87c6e89bc6ba873db212411a03fb7190ca79788
Gautam Mittal <gautam@mittal.net>
2018-07-05 10:38:20 -0700

    Load tokenizer.

--- evaluate.py
+++ evaluate.py
@@ -12,31 +12,22 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 from util.tokenizer_helpers import *
 
-# Load the training and test data
-train_data = pd.read_csv('data/train.csv')
+# Rebuild saved tokenizer
+tokenizer = load_tokenizer('save/tokenizer.pickle')
+
+# Load the test data
 test_data = pd.read_csv('data/test.csv')
-train_data = train_data.fillna(' ')
 test_data = test_data.fillna(' ')
-y = train_data['label'].values # Class values
-
-train_sent = train_data['title'] + ' ' + train_data['author'] + ' ' + train_data['text']
 test_sent = test_data['title'] + ' ' + test_data['author'] + ' ' + test_data['text']
-
-tokenizer = Tokenizer(num_words=20000, lower=True)
-tokenizer.fit_on_texts(list(train_sent))
-train_tokens = tokenizer.texts_to_sequences(train_sent)
 test_tokens = tokenizer.texts_to_sequences(test_sent)
-save_tokenizer('save/tokenizer.pickle', tokenizer)
-
-train = pad_sequences(train_tokens, maxlen=1000)
 test = pad_sequences(test_tokens, maxlen=1000)
 
-# model = load_model('save/model.h5')
-#
-# results = model.predict(test)
-# results = np.round(results)
-# results = results.reshape(results.shape[0])
-# results = pd.Series(results,name="label")
-# prediction = pd.concat([pd.Series(range(20800, 26000), name='id'),results], axis=1)
-# prediction['label'] = prediction['label'].map(int)
-# prediction.to_csv("submission.csv", index=False)
+model = load_model('save/model.h5')
+
+results = model.predict(test)
+results = np.round(results)
+results = results.reshape(results.shape[0])
+results = pd.Series(results,name="label")
+prediction = pd.concat([pd.Series(range(20800, 26000), name='id'),results], axis=1)
+prediction['label'] = prediction['label'].map(int)
+prediction.to_csv("submission.csv", index=False)
